f5170211884a48d2f42bb1115f78b3814a3b368b
Sunday Brian <brians931@gmail.com>
2019-08-22 15:03:25 +0300

    set up the firebase credentials in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { FIREBASE_CREDENTIALS } from './firebase.credentials';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(FIREBASE_CREDENTIALS)
+  ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,14 @@
+
+
+export const FIREBASE_CREDENTIALS={
+
+    apiKey: "AIzaSyBsnXYh1eftodsJs3PvdO1t_t9d2KjEbqo",
+    authDomain: "fir-weblearn-b660f.firebaseapp.com",
+    databaseURL: "https://fir-weblearn-b660f.firebaseio.com",
+    projectId: "fir-weblearn-b660f",
+    storageBucket: "fir-weblearn-b660f.appspot.com",
+    messagingSenderId: "654263767848",
+    appId: "1:654263767848:web:402c7dbcb6243f40"
+
+
+}
