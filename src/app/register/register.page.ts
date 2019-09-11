a8d285b56563e3c5fabb8102e8b39bd91a5f0b58
Sunday Brian <brians931@gmail.com>
2019-09-11 16:12:39 +0300

    add the router module to navigate a user to a login page after successful login

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app'
 import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -15,7 +17,7 @@ export class RegisterPage implements OnInit {
   password:string=""
   cpassword:string=""
 
-  constructor(public afAuth:AngularFireAuth,public alert:AlertController) { }
+  constructor(public afAuth:AngularFireAuth,public alert:AlertController,public route:Router) { }
 
   ngOnInit() {
   }
@@ -24,25 +26,32 @@ export class RegisterPage implements OnInit {
   async register(){
     const{ username,password,cpassword }=this
     if(password !==cpassword){
+      this.showAlert("Error","Passwords dont match")
       return console.error("Passwords dont match")
     }
 
 
     try {
       const res= await this.afAuth.auth.createUserWithEmailAndPassword(username,password)
+      this.showAlert("Succes","Welcome Aboard")
+      this.route.navigate(['/login'])
 
       
     } catch (error) {
       console.dir(error) 
+      this.showAlert("Error",error.message)
+
     } 
 
   }
 
   async showAlert(header:string,message:string){
-    const alert=this.alert.create({
+    const alert=await this.alert.create({
       header,
       message,
       buttons:["ok"]
     })
+
+    await alert.present()
   }
 }
