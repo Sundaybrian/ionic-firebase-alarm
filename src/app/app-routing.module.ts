4e9263084c0385137bc051144ec2531cc7f27155
Brian Sunday <brians931@gmail.com>
2020-01-13 21:31:54 +0300

    attached the authguard to the home and profile urls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+  canLoad: [AuthGuard]
+},
   // { path: 'alarm', loadChildren: './alarm/alarm.module#AlarmPageModule' },
   // { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   // { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule',
+  canLoad: [AuthGuard]
+ },
   // { path: 'logs', loadChildren: './logs/logs.module#LogsPageModule' },
   // { path: 'alarm', loadChildren: './home/alarm/alarm.module#AlarmPageModule' },
   // { path: 'logss', loadChildren: './home/logss/logss.module#LogssPageModule' },
