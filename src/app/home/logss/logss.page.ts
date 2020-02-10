108160baddf544ca3be2693253185eb5a0e6b8cf
Brian Sunday <brians931@gmail.com>
2020-02-10 16:34:29 +0300

    fixed the query of alarms logs and their display

--- src/app/home/logss/logss.page.ts
+++ src/app/home/logss/logss.page.ts
@@ -1,51 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-logss',
   templateUrl: './logss.page.html',
-  styleUrls: ['./logss.page.scss'],
+  styleUrls: ['./logss.page.scss']
 })
 export class LogssPage implements OnInit {
-
-  myAlarms: any[] = [];
-  myAlarmDates: any[] = [];
+  myAlarmsObjs: any[] = [];
+  myAlarmDatesData: Observable<any[]>;
   userId: any;
-  userAlarmRef;
   // testAlarm:Observable<any[]>=[]
 
   constructor(
     private afdb: AngularFireDatabase,
     public afAuth: AngularFireAuth
-    ) {
-
-      this.getMyAlarms();
-    }
+  ) {}
 
   ngOnInit() {
+    this.getMyAlarms();
   }
 
   getMyAlarms() {
-
     this.userId = this.afAuth.auth.currentUser.uid;
 
-    this.userAlarmRef = this.afdb.object('UserAlarmLogs/'+this.userId).valueChanges();
-
-    this.userAlarmRef.subscribe(dates => {
-      for (const key in dates) {
-        if (dates.hasOwnProperty(key)) {
-          // const element = object[key];
-          console.log(dates[key], 'geniussssssssssss');
-          // push the dates
-          this.myAlarmDates.push(key);
-
-          //
-          this.myAlarms.push(dates[key]);
-        }
-      }
-    });
-
+    this.myAlarmDatesData = this.afdb
+      .list('UserAlarmLogs/' + this.userId)
+.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
   }
-
 }
